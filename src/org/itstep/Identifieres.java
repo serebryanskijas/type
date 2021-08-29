2a88222f305d819cd58cbcd695e8ca293c71f73a
alexs <vorgen95@gmail.com>
2021-08-29 21:31:53 +0300

    lesson2

---
+++ src/org/itstep/Identifieres.java
@@ -0,0 +1,57 @@
+package org.itstep;
+
+public class Identifieres {
+    private static final Object __ = ;
+
+    public static void main(String[] args) {
+        /*
+        abcd
+        efg
+        hijk
+        lmnop
+        qrs
+        tuv
+        wx
+        yz
+         */
+//        Объявить переменные, необходимые для вычисления стоимости покупки, состоящей из нескольких тетрадей, карандашей и линейки
+        double totalsum, notebook, pricenotebook, pensil, pricepensil, ruller, priseruller;
+        int countnotebook, countpensil, countruller;
+
+//        В программе нужно зафиксировать какое-то состояние
+
+        boolean state;
+
+
+        /* масса стального куба.
+        int a, ro, mass;
+        a =1; ro=7800;
+        mass = (int) (ro * Math.pow(a,3));
+        System.out.println(mass);*/
+
+//        Задать произвольные букву и строку
+        char c = 'a';
+        String s = "Hello";
+        System.out.println(s);
+
+//        Масса электрона равна 9,1093837015⋅10^−31 кг и Масса Земли равна 5.972⋅10^24 кг
+        double massElectron = 9.1093837015e-31;
+        double massEarth = 5.972e24;
+        System.out.println(massElectron);
+        System.out.println(massEarth);
+
+//        Популяция Земли равна 7674538134
+        long l = 7_674_538_134l;
+        System.out.println(l);
+
+//        При нажатии на кнопку загорается светодиод (buttonState, ledState)|
+        boolean buttonState, ledState;
+
+//        Цифру числа нужно преобразовать в букву
+        int number, dijet;
+        char letter;
+
+
+    }
+
+}
---
+++ src/org/itstep/Main.java
@@ -0,0 +1,18 @@
+package org.itstep;
+
+import java.util.regex.Matcher;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double n;
+        n = 1.0/3.0;
+        System.out.println(n);
+    double y;
+//        y = Math.sin(Math.PI/2);
+          y = 2./0;
+          y=y*0;
+        System.out.println(y);
+    }
+
+}
